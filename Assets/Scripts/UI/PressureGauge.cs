f80a99ba95721224dafe6f8da72082a096469717
sirpotatosalad <timothy.a.anoso@gmail.com>
2025-01-25 23:52:33 +0800

    basic UI functionalities
    - oxygen warning pulses when low bubble count
    - bubble count affects bubble meter fill value

---
+++ Assets/Scripts/UI/PressureGauge.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PressureGauge : MonoBehaviour
+{
+
+    public Image bubbleGauge_WarningLight;
+    public Image bubbleGauge_BubbleLevel;
+
+    public TextMeshProUGUI bubbleGauge_BubbleLevelText;
+    public TextMeshProUGUI digitalTimer_TimerText;
+
+    public int currentBubbleCount;
+    public int maxBubbleCount = 25;
+
+    public float warningLightActivationPercentage = 0.45f;
+
+    private float bubblePercentage;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentBubbleCount = maxBubbleCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bubblePercentage = (float)currentBubbleCount / maxBubbleCount;
+
+        HandleBubbleLevel();
+        HandleWarningLight();
+    }
+
+    void HandleBubbleLevel()
+    {
+        if (bubbleGauge_BubbleLevel == null || bubbleGauge_BubbleLevelText == null) return;
+
+        bubbleGauge_BubbleLevel.fillAmount = bubblePercentage;
+        bubbleGauge_BubbleLevelText.text = currentBubbleCount.ToString();
+    }
+
+    void HandleWarningLight()
+    {
+        if (bubbleGauge_WarningLight == null) return;
+
+        if (bubblePercentage <= warningLightActivationPercentage) 
+        {
+            Color lightColour = bubbleGauge_WarningLight.color;
+            lightColour.a = Mathf.Lerp(0.2f, 1f, Mathf.PingPong(Time.time, 1));
+            bubbleGauge_WarningLight.color = lightColour;
+        }
+        else 
+        {
+            Color lightColour = bubbleGauge_WarningLight.color;
+            lightColour.a = 0;
+            bubbleGauge_WarningLight.color = lightColour;
+        }
+        
+    }
+}
