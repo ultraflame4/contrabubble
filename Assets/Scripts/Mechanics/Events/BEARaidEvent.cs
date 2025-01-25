aabc8ad9a9545f1f6ab72ed89bf51cf7a1da0a45
glenn-chann <105274111+glenn-chann@users.noreply.github.com>
2025-01-26 02:00:24 +0800

    sleep

--- Assets/Scripts/Mechanics/Events/BEARaidEvent.cs
+++ Assets/Scripts/Mechanics/Events/BEARaidEvent.cs
@@ -1,22 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class BEARaidEvent : MonoBehaviour
 {
-    [SerializeField] private GameObject diverPrefabs;
-    [SerializeField] private GameObject boatPrefabs;
+    [Header("Prefabs")]
+    [SerializeField] private GameObject diverPrefab;
+    [SerializeField] private GameObject boatPrefab;
 
     [Header("Event Parameter")]
-    [SerializeField] private int noOfBoats;
-    [SerializeField] private int noOfDiverPerBoats;
+    [SerializeField] private int noOfBoats = 3;
+    [SerializeField] private int noOfDiversPerBoats = 6;
+    [SerializeField] private float eventDuration = 30f;
+    [Tooltip("Distance from the centre of GameObject the boats are allowed to stop at")]
+    [SerializeField] private float dockableArea = 50f;
+    [Tooltip("Distance from the dockableArea where the boats will instantiate")]
+    [SerializeField] private float spawnDist = 10f;
+    [SerializeField] private float boatSpeed = 5f;
+    [SerializeField] private float diverDropInterval = 1.5f;
 
+    private List<GameObject> activeBoats = new List<GameObject>();
 
     public void Update()
     {
-
+        if (Input.GetMouseButton(0)) 
+        {
+            TriggerBEARaid();
+        }
     }
 
     public void TriggerBEARaid() 
     {
+        StartCoroutine(SpawnBoats());
+    }
 
+    private IEnumerator SpawnBoats()
+    {
+        yield return null;
     }
 }
