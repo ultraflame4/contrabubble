580eac9e781c9d2cc562cbca09a3a1fbadc7e0d1
glenn-chann <105274111+glenn-chann@users.noreply.github.com>
2025-01-26 00:18:20 +0800

    air drop event + all abilities created
    created =/= implemented

---
+++ Assets/Scripts/Mechanics/Events/PowerUps/ScriptableObjects/HackerBug.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HackerBug", menuName = "Scriptable Objects/HackerBug")]
+public class HackerBug : PowerUp
+{
+    public float amount;
+    public float duration;
+
+    public override void Apply(GameObject target)
+    {
+        PowerUpManager manager = target.GetComponent<PowerUpManager>();
+        if (manager != null) {
+            manager.TrackPowerUp(this, target, duration);
+        }
+        throw new NotImplementedException();
+    }
+
+    public override void Remove(GameObject target)
+    {
+        throw new NotImplementedException();
+    }
+}
