f7cfaa3b354aa1b52bd15ef7559ebec2c0fd3c1d
Samuel Ting <tingjs05@gmail.com>
2025-01-26 10:07:18 +0800

    bubble storage network behaviour

--- Assets/Scripts/Mechanics/BubbleStorage.cs
+++ Assets/Scripts/Mechanics/BubbleStorage.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class BubbleStorage : MonoBehaviour
+public class BubbleStorage : NetworkBehaviour
 {
     [SerializeField] private float maxLimit = Mathf.Infinity;
 
-    private float _bubbles;
+    private NetworkVariable<float> _bubbles = new NetworkVariable<float>(0f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     public float Bubbles
     {
-        get { return _bubbles; }
+        get { return _bubbles.Value; }
         set
         {
-            _bubbles = Mathf.Clamp(value, 0f, maxLimit);
+            _bubbles.Value = Mathf.Clamp(value, 0f, maxLimit);
         }
     }
 }
